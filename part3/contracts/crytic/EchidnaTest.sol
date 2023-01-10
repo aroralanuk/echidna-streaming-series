1b345ec081ec53c3d4346d0775d12c29bf3bb55d
Kunal Arora <kuarora@ucsd.edu>
2023-01-09 22:55:10 -0800

    test more money more lp

--- part3/contracts/crytic/EchidnaTest.sol
+++ part3/contracts/crytic/EchidnaTest.sol
@@ -4,52 +4,102 @@ import "./Setup.sol";
 
 contract EchidnaTest is Setup {
     event logUints(uint unit1, uint unit2);
-    function testProvideLiquidity(uint amount1, uint amount2) public {
-        //Preconditions: 
+    event BalancePair(uint r0, uint r1);
+    // function testProvideLiquidity(uint amount1, uint amount2) public {
+    //     //Preconditions:
+    //     amount1 = _between(amount1, 1000, uint(-1));
+    //     amount2 = _between(amount2, 1000, uint(-1));
+
+    //     if(!completed) {
+    //         _init(amount1, amount2);
+    //     }
+    //     uint lpTokenBalanceBefore = pair.balanceOf(address(userA));
+    //     (uint reserve0Before, uint reserve1Before,) = pair.getReserves();
+    //     uint kBefore = reserve0Before * reserve1Before;
+
+    //     (bool success1,) = userA.proxy(address(testToken1),abi.encodeWithSelector(testToken1.transfer.selector, address(pair),amount1));
+    //     (bool success2,) = userA.proxy(address(testToken2),abi.encodeWithSelector(testToken2.transfer.selector, address(pair),amount2));
+    //     require(success1 && success2);
+
+    //     //Action:
+    //     (bool success3,) = userA.proxy(address(pair),abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(userA)));
+
+    //     //Postconditions:
+    //     if(success3) {
+    //         uint lpTokenBalanceAfter = pair.balanceOf(address(userA));
+    //         (uint reserve0After, uint reserve1After,) = pair.getReserves();
+    //         uint kAfter = reserve0After * reserve1After;
+    //         assert(lpTokenBalanceBefore < lpTokenBalanceAfter);
+    //         assert(kBefore < kAfter);
+
+    //     }
+
+    // }
+
+    function testProvideMoreLiquidity(uint amount1, uint amount2) public {
+        //Preconditions:
         amount1 = _between(amount1, 1000, uint(-1));
         amount2 = _between(amount2, 1000, uint(-1));
+        uint amount3 = (amount2 * 90)/100;
 
         if(!completed) {
             _init(amount1, amount2);
+            _init(address(userB), amount1, amount3);
         }
-        uint lpTokenBalanceBefore = pair.balanceOf(address(user));
+        uint lpTokenBalanceBeforeA = pair.balanceOf(address(userA));
         (uint reserve0Before, uint reserve1Before,) = pair.getReserves();
         uint kBefore = reserve0Before * reserve1Before;
 
-        (bool success1,) = user.proxy(address(testToken1),abi.encodeWithSelector(testToken1.transfer.selector, address(pair),amount1));
-        (bool success2,) = user.proxy(address(testToken2),abi.encodeWithSelector(testToken2.transfer.selector, address(pair),amount2));
-        require(success1 && success2);
+        // Action:
+        (bool successA1,) = userA.proxy(address(testToken1),abi.encodeWithSelector(testToken1.transfer.selector, address(pair),amount1));
+        (bool successA2,) = userA.proxy(address(testToken2),abi.encodeWithSelector(testToken2.transfer.selector, address(pair),amount2));
+        require(successA1 && successA2);
+
+        (bool successA3,) = userB.proxy(address(pair),abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(userA)));
+
+        (uint reserve0After, uint reserve1After,) = pair.getReserves();
+
+        emit BalancePair(reserve0After - reserve0Before, reserve1After - reserve1Before);
+
+        (reserve0Before, reserve1Before) = (reserve0After, reserve1After);
+
+        (bool successB1,) = userB.proxy(address(testToken1),abi.encodeWithSelector(testToken1.transfer.selector, address(pair),amount1));
+        (bool successB2,) = userB.proxy(address(testToken2),abi.encodeWithSelector(testToken2.transfer.selector, address(pair),amount3));
+        require(successB1 && successB2);
 
         //Action:
-        (bool success3,) = user.proxy(address(pair),abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(user)));
-        
+        (bool successB3,) = userB.proxy(address(pair),abi.encodeWithSelector(bytes4(keccak256("mint(address)")), address(userB)));
+
+        (reserve0After, reserve1After,) = pair.getReserves();
+        emit BalancePair(reserve0After - reserve0Before, reserve1After - reserve1Before);
+
         //Postconditions:
-        if(success3) {
-            uint lpTokenBalanceAfter = pair.balanceOf(address(user));
-            (uint reserve0After, uint reserve1After,) = pair.getReserves();
-            uint kAfter = reserve0After * reserve1After;
-            assert(lpTokenBalanceBefore < lpTokenBalanceAfter);
-            assert(kBefore < kAfter);
+        if(successA3 && successB3) {
+            uint lpTokenBalanceAfterA = pair.balanceOf(address(userA));
+            uint lpTokenBalanceAfterB = pair.balanceOf(address(userB));
+            emit logUints(lpTokenBalanceAfterA, lpTokenBalanceAfterB);
 
+            assert(lpTokenBalanceAfterA >= lpTokenBalanceAfterB);
         }
 
     }
-    function testSwap(uint amount1, uint amount2) public {
-        
-        if(!completed) {
-            _init(amount1, amount2);
-        }
-        
-        //Preconditions
-        pair.sync(); // we matched the balances with reserves
-        require(pair.balanceOf(address(user)) > 0); //there is liquidity for the swap
-        //Call:
-        (bool success1,) = user.proxy(address(pair), abi.encodeWithSelector(pair.swap.selector, amount1,amount2,address(user),""));
 
-        //Postcondition:
-        assert(!success1); //call should never succeed
+    // function testSwap(uint amount1, uint amount2) public {
+
+    //     if(!completed) {
+    //         _init(amount1, amount2);
+    //     }
+
+    //     //Preconditions
+    //     pair.sync(); // we matched the balances with reserves
+    //     require(pair.balanceOf(address(user)) > 0); //there is liquidity for the swap
+    //     //Call:
+    //     (bool success1,) = user.proxy(address(pair), abi.encodeWithSelector(pair.swap.selector, amount1,amount2,address(user),""));
+
+    //     //Postcondition:
+    //     assert(!success1); //call should never succeed
 
 
-    
-    }
+
+    // }
 }
--- part3/contracts/crytic/Setup.sol
+++ part3/contracts/crytic/Setup.sol
@@ -15,28 +15,38 @@ contract Setup {
     UniswapV2Pair pair;
     UniswapV2ERC20 testToken1;
     UniswapV2ERC20 testToken2;
-    Users user;
+    Users userA;
+    Users userB;
     bool completed;
-    
+
     constructor() public {
         testToken1 = new UniswapV2ERC20();
         testToken2 = new UniswapV2ERC20();
         factory = new UniswapV2Factory(address(this));
         address testPair = factory.createPair(address(testToken1), address(testToken2));
         pair = UniswapV2Pair(testPair);
-        user = new Users();
-        user.proxy(address(testToken1),abi.encodeWithSelector(testToken1.approve.selector, address(pair),uint(-1)));
-        user.proxy(address(testToken2), abi.encodeWithSelector(testToken2.approve.selector,address(pair),uint(-1)));
+
+        userA = new Users();
+        userA.proxy(address(testToken1),abi.encodeWithSelector(testToken1.approve.selector, address(pair),uint(-1)));
+        userA.proxy(address(testToken2), abi.encodeWithSelector(testToken2.approve.selector,address(pair),uint(-1)));
+
+        userB = new Users();
+        userB.proxy(address(testToken1),abi.encodeWithSelector(testToken1.approve.selector, address(pair),uint(-1)));
+        userB.proxy(address(testToken2), abi.encodeWithSelector(testToken2.approve.selector,address(pair),uint(-1)));
     }
 
     function _init(uint amount1, uint amount2) internal {
-        testToken1.mint(address(user), amount1);
-        testToken2.mint(address(user), amount2);
+        _init(address(userA), amount1, amount2);
+    }
+
+    function _init(address user_, uint amount1, uint amount2) internal {
+        testToken1.mint(user_, amount1);
+        testToken2.mint(user_, amount2);
         completed = true;
     }
 
     function _between(uint val, uint low, uint high) internal pure returns(uint) {
-        return low + (val % (high-low +1)); 
+        return low + (val % (high-low +1));
     }
 
 
